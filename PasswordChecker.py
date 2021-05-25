384e7eb4d12e16a9237fe41726f573e0d9858a70
Anton Mett <antonmett@gmail.com>
2021-05-25 12:25:45 +0300

    Rebuilt generator.

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -1,35 +1,42 @@
-import string
 import random
 import re
 from fpdf import FPDF
+from os import remove
 
 password_list = []
 
 
-def password_generator(size=4, chars=string.digits):
-    return ''.join(random.choice(chars)for _ in range(size))
+def password_generator():
+    security_number = []
+    password_string = ''
+    security_number.append(random.randrange(3, 10))
+    security_number.append(random.randrange(0, 10))
+    if security_number[0] != security_number[1]:
+        security_number.append(random.randrange(0, 10))
+        if security_number[1] != security_number[2]:
+            security_number.append(random.randrange(0, 10))
+            if security_number[2] != security_number[3]:
+                pass
+    if len(security_number) == 4:
+        password_string = (
+            f'{security_number[0]}{security_number[1]}{security_number[2]}{security_number[3]}')
+    return(password_string)
 
 
-for x in range(1000000):
-    print(f'PASSWORD LIST LENGHT IS {len(password_list)}')
+for x in range(10000000):
+
     password = password_generator()
 
-    if re.fullmatch(r"^.*(?=.*?[0-9])(?=.*?[8])(?=.*?[7]).{4,}.*$", password):
-        if password[0] == str(1):
-            pass
-        elif password[0] == str(2):
-            pass
-        elif password[1] == str(1):
-            pass
-        elif password[1] == str(2):
-            pass
-        else:
-            if password not in password_list:
-                password_list.append(password)
-                print(password)
-            else:
-                pass
+    if re.fullmatch(r"^.*(?=.*?[0-9])(?=.*?[8])(?=.*?[7]).*$", password):
 
+        if password not in password_list:
+            password_list.append(password)
+            print(len(password_list))
+
+print(f'PASSWORD LIST LENGHT IS {len(password_list)}')
+
+password_list.sort()
+print(password_list)
 with open('readme.txt', 'w',) as f:
     f.write('    '.join(password_list))
 pdf = FPDF()
@@ -40,5 +47,6 @@ for x in text_file:
     pdf.multi_cell(0, 8, txt=x)
 pdf.set_font('helvetica', size=30)
 pdf.ln(6)
-pdf.cell(0, 8, 'GOOD LUCK MARINA! :D:D:D', 0, 0, 'C')
+pdf.cell(0, 8, 'GOOD LUCK MOM! :D:D:D', 0, 0, 'C')
 pdf.output('password.pdf')
+remove('readme.txt')
