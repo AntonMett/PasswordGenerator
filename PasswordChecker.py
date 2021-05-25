214f0142bf60bf3089453b65ebd0c58e40424766
Anton Mett <antonmett@gmail.com>
2021-05-25 12:44:52 +0300

    Fixed 4th number.

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -1,7 +1,6 @@
 import random
 import re
 from fpdf import FPDF
-from os import remove
 
 password_list = []
 
@@ -15,15 +14,15 @@ def password_generator():
         security_number.append(random.randrange(0, 10))
         if security_number[1] != security_number[2]:
             security_number.append(random.randrange(0, 10))
-            if security_number[2] != security_number[3]:
-                pass
+            if security_number[2] == security_number[3]:
+                security_number = []
     if len(security_number) == 4:
         password_string = (
             f'{security_number[0]}{security_number[1]}{security_number[2]}{security_number[3]}')
     return(password_string)
 
 
-for x in range(10000000):
+for x in range(1000000):
 
     password = password_generator()
 
@@ -49,4 +48,3 @@ pdf.set_font('helvetica', size=30)
 pdf.ln(6)
 pdf.cell(0, 8, 'GOOD LUCK MOM! :D:D:D', 0, 0, 'C')
 pdf.output('password.pdf')
-remove('readme.txt')
